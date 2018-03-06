41aa066c77c624e47d919af082c9a6e5e2c6ed2c
Artur Czepiel <czepiel.artur@gmail.com>
2018-03-06 21:39:17 +0100

    initial support for ECB exchange rates

--- tests/test_invoices.py
+++ tests/test_invoices.py
@@ -9,14 +9,22 @@ from pytest import mark, raises
 
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.test.utils import override_settings
 
 from django_factory_boy import auth as auth_factories
 from freezegun import freeze_time
+import responses
 
 from assopy.models import Country, Invoice, Order, Vat, VatFare
 from assopy.tests.factories.user import UserFactory as AssopyUserFactory
 from conference.models import AttendeeProfile, Fare, Ticket
 from conference import settings as conference_settings
+from conference.exchangerates import (
+    DAILY_ECB_URL,
+    EXAMPLE_ECB_DAILY_XML,
+    get_ecb_rates_for_currency,
+    convert_from_EUR_using_latest_exrates
+)
 from email_template.models import Email
 
 from tests.common_tools import template_used, sequence_equals, serve  # NOQA
@@ -441,3 +449,33 @@ def test_if_invoice_stores_information_about_the_seller(client):
 
         response = client.get(invoice_url(invoice))
         assert "Edinburgh FIXME" in response.content.decode('utf-8')
+
+
+@mark.django_db
+@responses.activate
+# in this test we want to check if values are cached correctly...
+@override_settings(CACHES=settings.ENABLE_LOCMEM_CACHE)
+def test_exchange_rates_are_working():
+    """
+    https://github.com/EuroPython/epcon/issues/617
+    """
+
+    responses.add(responses.GET, DAILY_ECB_URL, body=EXAMPLE_ECB_DAILY_XML)
+    assert get_ecb_rates_for_currency("GBP") == (
+        # date is used in example xml, so it will be reutrned here as well
+        date(2018, 3, 6), Decimal('0.89165')
+    )
+    assert len(responses.calls) == 1
+
+    # second call will use cache instead of calling URL so the responses are
+    # still at 1.
+    assert get_ecb_rates_for_currency("JPY") == (
+        date(2018, 3, 6), Decimal('131.84')
+    )
+    assert len(responses.calls) == 1
+
+    a = convert_from_EUR_using_latest_exrates(Decimal("10"), "GBP")
+    b = {'converted': Decimal('11.22'),
+         'using_exrate_date': date(2018, 3, 6),
+         'exrate': Decimal('0.89165')}
+    assert a == b
